4882de6f3ac9ccc95ed8cf6390f7e08468148125
amirrezapanahi <amirpanahi11@gmail.com>
2024-10-20 22:06:28 +0100

    more build?

--- src/Library/obj/Debug/net8.0/Library.AssemblyInfo.fs
+++ src/Library/obj/Debug/net8.0/Library.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da1d51812f638c301ef277285b1a1edcd58d290b")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Library")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
